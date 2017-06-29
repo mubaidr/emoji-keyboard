e66307990a8e3cd4a778ec959c12b4f3a7c9485b
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-06-29 15:58:55 +0500

    add recent emoji storage functions

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import _ from 'lodash'
 import emojiData from './../data/emojis.json'
 
-/* eslint-disable no-new */
+/* eslint-disable no-new, no-undef */
+
 new Vue({
   el: '#emoji-app',
   data () {
@@ -23,7 +24,9 @@ new Vue({
     }
   },
   created () {},
-  mounted () {},
+  mounted () {
+    this.initializeRecentEmoji()
+  },
   methods: {
     getSkinTones () {
       return _.orderBy(_.filter(emojiData, (emoji) => {
@@ -38,11 +41,40 @@ new Vue({
         emoji.text ? emoji.text.toLowerCase().includes(txt) : false ||
         emoji.texts ? (emoji.texts.indexOf(txt) > -1) : false
     },
-    selectEmoji (emoji) {
+    selectEmoji (emoji, isRecent) {
       this.emojiSelected = emoji.unified
+      if (!isRecent) {
+        this.addRecentEmoji(emoji)
+      }
     },
     showemoji (emoji) {
       return '_' + emoji.unified
+    },
+    /* chrome utilties */
+    clearChromeStorage (callback) {
+      chrome.storage.local.clear(callback)
+    },
+    initializeRecentEmoji (callback) {
+      chrome.storage.local.get(null, (items) => {
+        if (items && items.emojis) {
+          if (items.emojis.length > 0) {
+            this.emojisRecent = items.emojis
+          }
+        } else {
+          chrome.local.storage.set('emojis', [], callback)
+        }
+      })
+    },
+    addRecentEmoji (emoji, callback) {
+      chrome.storage.local.get(null, (items) => {
+        let emojis = items.emojis
+        if (items.emojis.length === 14) {
+          emojis.pop()
+        }
+        emojis.unshift(emoji)
+        this.emojisRecent = emojis
+        chrome.local.storage.set('emojis', emojis, callback)
+      })
     }
   }
 })
