694520b9318e1da2a94daf240f2a7076c612a659
mubaidr <mubaidr@gmail.com>
2017-07-03 11:08:35 +0500

    add contenteditable support

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -8,20 +8,21 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let key in changes) {
       let emoji = changes[key].newValue[0]
       if (emoji) {
-        let text = window.___emojikeyboard_element.value
-        let selStart = window.___emojikeyboard_element.selectionStart
-        let selEnd = window.___emojikeyboard_element.selectionEnd
-        window.___emojikeyboard_element.value = text.slice(0, selStart) + String.fromCodePoint('0x' + emoji.unified) + text.slice(selEnd, text.length)
+        if (window.___emojikeyboard_element.isContentEditable) {
+          window.___emojikeyboard_element.innerHTML += String.fromCodePoint('0x' + emoji.unified)
+        } else {
+          let text = window.___emojikeyboard_element.value
+          let selStart = window.___emojikeyboard_element.selectionStart
+          let selEnd = window.___emojikeyboard_element.selectionEnd
+          window.___emojikeyboard_element.value = text.slice(0, selStart) + String.fromCodePoint('0x' + emoji.unified) + text.slice(selEnd, text.length)
+        }
       }
     }
   }
 })
 
-$('input[type="text"], textarea, div[contenteditable="true"], p[contenteditable="true"]').forEach(function (element) {
+$('input[type="text"], textarea, [contenteditable="true"]').forEach(function (element) {
   element.onfocus = function () {
     window.___emojikeyboard_element = this
-  }
-  element.onblur = function () {
-    // window.___emojikeyboard_element = null
   }
 }, this)
